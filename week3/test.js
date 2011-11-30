a9c94d3ba6afea076a7eb11ae59eb91bd85a18d0
Toon-ABCTech <suphareuk@gmail.com>
2011-11-30 15:42:48 +0700

    fix Test 1, Test 2 fixing wrong data type check

--- week3/test.js
+++ week3/test.js
@@ -1,6 +1,6 @@
 module('Module A');  
 test('Test for function fizzBuzz() exists', function() { 
-	ok(fizzBuzz() == fizzBuzz(), "ok function fizzBuzz() exists");
+	ok(fizzBuzz(1) == fizzBuzz(1), "ok function fizzBuzz() exists");
 });
 
 module('Module B');
@@ -8,6 +8,6 @@ test('Test pass parameter to function fizzBuzz() expect return value', function(
 	ok(fizzBuzz(1) == 1, "expect return value");
 });
 test('Test pass parameter to function fizzBuzz() expect only number value', function() { 
-	ok(fizzBuzz("1") != "1", "expect number value");
-    ok(fizzBuzz("5") != "5", "expect number value");
+	ok(fizzBuzz("1") == 1, "expect number value");
+    ok(fizzBuzz("5") == 5, "expect number value");
 });
