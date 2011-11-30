0efa9d3eb7a078e033667e0fd7edfb98dc98c4ff
Toon-ABCTech <suphareuk@gmail.com>
2011-11-30 16:04:59 +0700

    fix Test 3 and add more test to ensure that check to possible data type

--- week3/test.js
+++ week3/test.js
@@ -8,6 +8,7 @@ test('Test pass parameter to function fizzBuzz() expect return value', function(
 	ok(fizzBuzz(1) == 1, "expect return value");
 });
 test('Test pass parameter to function fizzBuzz() expect only number value', function() { 
-	ok(fizzBuzz("1") == 1, "expect number value");
-    ok(fizzBuzz("5") == 5, "expect number value");
+	ok(fizzBuzz(10) === 10, "expect number value");
+    ok(fizzBuzz("5") === 5, "expect number value");
+
 });
