98826bc7c428fa973148703bc487bbb141b28f41
Toon-ABCTech <suphareuk@gmail.com>
2011-11-30 15:10:27 +0700

    Test 2 : Test return value from function fizzBuzz()

--- week3/test.js
+++ week3/test.js
@@ -2,3 +2,8 @@ module('Module A');
 test('Test for function fizzBuzz() exists', function() { 
 	ok(fizzBuzz() == fizzBuzz(), "ok function fizzBuzz() exists");
 });
+
+module('Module B');
+test('Test pass parameter to function fizzBuzz() expect return value', function() { 
+	ok(fizzBuzz(1) == 1, "expect return value");
+});
